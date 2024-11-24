e607e9964d4280a3f33260f21280e867988149eb
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-11-24 16:59:25 -0500

    Gray out slider when it is unused

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ $(document).ready(function () {
     $slider.on('input', function () {
       const duration = $(this).val();
       $frame.css('animation-duration', `${duration}s`);
-      $label.text(`Animation Duration: ${duration}s`);
+      $label.text(`Breath Duration: ${duration}s`);
     });
   
     // Set the default animation duration
     $frame.css('animation-duration', `${$slider.val()}s`);
+
+  let timeout;
+  const greyOutSlider = () => {
+    $slider.css('opacity', 0.1);
+    $label.css('opacity', 0.1);
+  };
+
+  const restoreSlider = () => {
+    clearTimeout(timeout);
+    $slider.css('opacity', 1);
+    $label.css('opacity', 1);
+    timeout = setTimeout(greyOutSlider, 2000);
+  };
+
+  $slider.on('mouseover input', restoreSlider);
+
+  timeout = setTimeout(greyOutSlider, 2000);
   });
   
